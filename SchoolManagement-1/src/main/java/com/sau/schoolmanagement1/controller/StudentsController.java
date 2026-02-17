3833f448065f4bc2794351acf007b3a91620f29a
yasinsblk <yasinsebelek61@gmail.com>
2026-02-18 00:32:26 +0300

    Student table and controller completed

--- SchoolManagement-1/src/main/java/com/sau/schoolmanagement1/controller/StudentsController.java
+++ SchoolManagement-1/src/main/java/com/sau/schoolmanagement1/controller/StudentsController.java
@@ -2,13 +2,15 @@ package com.sau.schoolmanagement1.controller;
 
 import com.sau.schoolmanagement1.db.StudentCrudOperations;
 import com.sau.schoolmanagement1.dto.Students;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.util.Optional;
 
 public class StudentsController {
@@ -40,15 +42,10 @@ public class StudentsController {
     private Button clearStudent;
 
 
-    @FXML
-    void clearStudent () {
-        studentId.setText("");
-        studentName.setText("");
-        studentDepartment.setText("");
-    }
+
 
     @FXML
-    public void getStudent (ActionEvent event) {
+    void getStudent (ActionEvent event) {
         checkId(studentId.getText(), event);
         StudentCrudOperations studentCrudOperations = new StudentCrudOperations();
         int id = Integer.parseInt(studentId.getText());
@@ -69,10 +66,79 @@ public class StudentsController {
     }
 
     @FXML
-    public void updateStudent () {
+    void updateStudent (ActionEvent event) {
+        checkId(studentId.getText(), event);
+        Students students = new Students();
+        students.setName(studentName.getText());
+        students.setDepartment(studentDepartment.getText());
+        students.setId(Integer.parseInt(studentId.getText()));
+        StudentCrudOperations studentCrudOperations = new StudentCrudOperations();
+        int res = studentCrudOperations.updateStudents(students);
+        if(res > 0){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Student with id " + studentId.getText() + " id updated");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error on update student!");
+            alert.showAndWait();
+        }
 
     }
 
+    @FXML
+    void saveStudent (ActionEvent event) {
+        checkId(studentId.getText(), event);
+        Students students = new Students();
+        students.setName(studentName.getText());
+        students.setDepartment(studentDepartment.getText());
+        students.setId(Integer.parseInt(studentId.getText()));
+        StudentCrudOperations studentCrudOperations = new StudentCrudOperations();
+        int res = studentCrudOperations.insertStudents(students);
+        if(res > 0){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Student with id " + studentId.getText() + " saved");
+            alert.showAndWait();
+        } else if(res == -1){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("There is another student with id: " + studentId.getText());
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error on saving student!");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    void deleteStudent (ActionEvent event) {
+        checkId(studentId.getText(), event);
+        StudentCrudOperations studentCrudOperations = new StudentCrudOperations();
+        int id = Integer.parseInt(studentId.getText());
+        int result = studentCrudOperations.deleteStudentsById(id);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText("Students with id " + studentId.getText() + " deleted");
+        alert.showAndWait();
+        clearStudent(event);
+    }
+
+    @FXML
+    public void close(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    void clearStudent (ActionEvent event) {
+        studentId.setText("");
+        studentName.setText("");
+        studentDepartment.setText("");
+    }
     public void checkId (String id, ActionEvent event){
 
     }
--- SchoolManagement-1/src/main/java/module-info.java
+++ SchoolManagement-1/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.sau.group9.schoolmanagement1 {
+module com.sau.schoolmanagement1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
 
 
-    opens com.sau.schoolmanagement1 to javafx.fxml;
+    opens com.sau.schoolmanagement1.controller to javafx.fxml;
     exports com.sau.schoolmanagement1;
 }
